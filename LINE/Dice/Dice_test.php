60b2d8a83419f3212ebbeed70908905279cbe013
IceinflameYeh <97699713+IceinflameYeh@users.noreply.github.com>
2022-03-27 01:37:33 +0800

    Update Dice_test.php

--- LINE/Dice/Dice_test.php
+++ LINE/Dice/Dice_test.php
@@ -1,3 +1,18 @@
 <?php
-
+a="abcdefghijklmnopqrstuvwxyz"
+b="ɾ⦣ᶖ⟍̅ᒷ⎓˧⍑¦⫶ꖌꖎᒲ⧶੭ⅰ!ᑑ∷ᓭ⅂⚍⍊∴˙⧸ǁሰ"
+c=input("Type it out:")
+d=""
+for i in range(len(c)-1):
+   if c[i] in a:
+      for j in range(len(a)-1):
+        if a[j]==c[i]:
+            d+=b[j]
+   elif c[i] in b:
+      for j in ange(len(b)-1)
+        if b[j]==c[j]:
+            d+=a[j]
+   else:
+       d+=c[i]
+print(d)
 ?>
